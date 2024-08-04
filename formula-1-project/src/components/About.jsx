b731c4fca8502c6369f8882da57fb03386bff9c0
Charlie Jay <charliejbjay@gmail.com>
2024-08-04 11:42:23 +1000

    implented additional css on NavBar.css and index.css. Installed Recharts

--- formula-1-project/src/components/About.jsx
+++ formula-1-project/src/components/About.jsx
@@ -1,7 +1,5 @@
 const About = () => {
-    return (
-        <h1>About F1 LapInsights</h1>
-    )
+    return <h1>About F1 LapInsights</h1>;
 };
 
 export default About;
--- formula-1-project/src/components/LandingPage.jsx
+++ formula-1-project/src/components/LandingPage.jsx
@@ -5,16 +5,16 @@ import "../components/LandingPage.css";
 const LandingPage = () => {
     return (
         <>
-        <div className="container">
-            <div className="Logo">
-                <h1>F1 LapInsights</h1>
+            <div className="container">
+                <div className="Logo">
+                    <h1>F1 LapInsights</h1>
+                </div>
+                <div>
+                    <p></p>
+                </div>
             </div>
-            <div>
-                <p></p>
-            </div>
-        </div>
         </>
-    )
-}
+    );
+};
 
 export default LandingPage;
